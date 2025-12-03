8b2193c4206af525c75525d4d022104a12936631
farhan5548 <farhanfareed450@gmail.com>
2025-12-03 11:50:23 +0500

    nothing more added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import { Box, Typography } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
@@ -6,9 +11,31 @@ const Footer = () => {
       <Box
         sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
       >
+        <Box
+          sx={{
+            m: 3,
+            "& svg": {
+              fontSize: "60px",
+              cursor: "pointer",
+              mr: 2,
+            },
+            "& svg:hover": {
+              color: "goldenrod",
+              transform: "translateX(5px)",
+              transition: "all 500ms",
+            },
+          }}
+        >
+          {/* icons */}
+          <InstagramIcon />
+          <TwitterIcon />
+          <GitHubIcon />
+          <YouTubeIcon />
+          <LinkedInIcon />
+        </Box>
         <Typography
           variant="h5"
-          sx={{ "@media (max-width:600px)": { fontSize: "1rem",  } }}
+          sx={{ "@media (max-width:600px)": { fontSize: "1rem" } }}
         >
           All Rights Reserved &copy; My Restaurant.
         </Typography>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
 import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
+import Kitchen from '../images/kitchen.jpg'
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="home" style={{backgroundImage:`url(${Kitchen})`}}>
+        <div className="headerContainer">
+          <h1>Food Website</h1>
+          <p>Best Food In Pakistan</p>
+          <Link to={"/menu"}>
+            <button>ORDER NOW</button>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 };
